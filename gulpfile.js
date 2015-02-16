c27c549a2414a232c4c8fcc1c582b92e5fc85863
Darek Rossman <darek_rossman@valpak.com>
2015-02-16 17:05:31 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var gutil = require('gulp-util');
+var es6ify = require('es6ify');
+var reactify = require('reactify');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('browsersync', ['bundle'], function(){
+  return browserSync({
+    server: {baseDir: './'},
+    files: ['**/js/bundle.js', '**/*.css', '**/*.html']
+  })  
+})
+
+gulp.task('bundle', function(){
+  var bundler = browserify(watchify.args);
+  bundler.add('./js/app.js');
+  bundler.transform('reactify');
+  bundler.transform('es6ify');
+  
+  if (global.isWatching) {
+    bundler = watchify(bundler);
+    bundler.on('update', bundle);
+  }
+
+  function bundle() {
+    gutil.log('Browserifying...')
+    return bundler.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .on('end', function(){
+        gutil.log('Finished browserifying')
+      })
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./js'));
+  }
+
+  return bundle();
+})
+
+gulp.task('sass', function () {
+  return gulp.src(['css/style.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css/'))
+});
+
+gulp.task('set-watching', function(){
+  global.isWatching = true;
+})
+
+
+gulp.task('watch', ['set-watching', 'bundle', 'browsersync'], function(){
+  gulp.watch('css/**/*.scss', ['sass']);
+}); 
+
+gulp.task('build', ['bundle', 'sass']);
---
+++ js/App.react.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Router = require('react-router');
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+ 
+  render: function() {
+    return (
+      <div>
+        <RouteHandler/>
+      </div>
+    ); 
+  }
+
+});
+
+module.exports = App;
